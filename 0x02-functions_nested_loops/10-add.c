7f383ad062ccb76c18d376fb3703dc49599168e4
AhmedOthman94 <othmanahmed7367@gmail.com>
2023-08-15 08:28:34 -0700

    Luck and Huge efforts

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @f: first input
+ * @s: second input
+ *
+ * Return: sum of f & s
+*/
+
+int add(int f, int s)
+{
+	int sum = f + s;
+
+	return (sum);
+}
