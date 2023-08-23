bae8e1ad6b887ddb5b02231b8c40e62960710834
AhmedOthman94 <othmanahmed7367@gmail.com>
2023-08-23 08:55:02 -0700

    Huge Big Hero

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,65 +1,78 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * infinite_add - add 2 strings.
- * @n1: string1.
- * @n2: string2.
- * @r: buffer
- * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
 */
+
+void rev_string(char *n)
+{
+	int a = 0;
+	int b = 0;
+	char t;
+
+	while (*(n + a) != '\0')
+	{
+		a++;
+	}
+	a--;
+
+	for (b = 0; b < a; b++, a--)
+	{
+		t = *(n + b);
+		*(n + b) = *(n + a);
+		*(n + a) = t;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int il = 0, jl = 0, ca = 0, i, j, sm, big;
+	int ow = 0, a = 0, b = 0, ds = 0;
+	int v1 = 0, v2 = 0, tt = 0;
 
-	while (n1[il] != '\0')
-	{
-		il++;
-	}
-	while (n2[jl] != '\0')
-	{
-		jl++;
-	}
-	if (il > jl)
-		big = il;
-	else
-		big = jl;
-	if ((big + 1) >= size_r)
+	while (*(n1 + a) != '\0')
+		a++;
+	while (*(n2 + b) != '\0')
+		b++;
+	a--;
+	b--;
+	if (b >= size_r || a >= size_r)
 		return (0);
-	r[big + 1] = '\0';
-
-	while (big >= 0)
+	while (b >= 0 || a >= 0 || ow == 1)
 	{
-		i = (n1[il - 1] - '0');
-		j = (n1[jl - 1] - '0');
-		if (il > 0 && jl > 0)
-			sm = i + j + ca;
-		else if (il < 0 && jl > 0)
-			sm = j + ca;
-		else if (il > 0 && jl < 0)
-			sm = i + ca;
+		if (a < 0)
+			v1 = 0;
 		else
-			sm = ca;
-
-		if (sm > 9)
-		{
-			ca = sm / 10;
-			sm = (sm % 10) + '0';
-		}
+			v1 = *(n1 + a) - '0';
+		if (b < 0)
+			v2 = 0;
 		else
-		{
-			ca = 0;
-			sm = sm + '0';
-		}
-		r[big] = sm;
-		il--;
-		jl--;
-		big--;
+			v2 = *(n2 + b) - '0';
+		tt = v1 + v2 + ow;
+		if (tt >= 10)
+			ow = 1;
+		else
+			ow = 0;
+		if (ds >= (size_r - 1))
+			return (0);
+		*(r + ds) = (tt % 10) + '0';
+		ds++;
+		b--;
+		a--;
 	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+	if (ds == size_r)
+		return (0);
+	*(r + ds) = '\0';
+	rev_string(r);
+	return (r);
 }
