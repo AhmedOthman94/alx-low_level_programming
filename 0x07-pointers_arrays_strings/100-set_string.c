4972f4bcb1bb268c3638ef3dffb89ba7b5fdccfc
AhmedOthman94 <othmanahmed7367@gmail.com>
2023-08-28 06:53:55 -0700

    Huge Efforts

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - redirect.
+ * @s: pointer to pointer
+ * @to: pointer to char
+ *
+ * Return: chessboard.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
