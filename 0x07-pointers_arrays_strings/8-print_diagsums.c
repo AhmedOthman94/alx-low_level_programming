1bb1756efd51a6edf105e09beaf68f724f3632dc
AhmedOthman94 <othmanahmed7367@gmail.com>
2023-08-28 06:46:36 -0700

    LUCK

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry point
+ * @a: matrix
+ * @size: size
+ * Return: Always 0 (Success)
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int s1, s2, t;
+
+	s1 = 0;
+	s2 = 0;
+
+	for (t = 0; t < size; t++)
+	{
+		s1 = s1 + a[t * size + t];
+	}
+
+	for (t = size - 1; t >= 0; t--)
+	{
+		s2 += a[t * size + (size - t - 1)];
+	}
+
+	printf("%d, %d\n", s1, s2);
+}
