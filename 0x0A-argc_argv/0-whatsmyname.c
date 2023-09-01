075632be0bd558bd247986ffefecc21a73d76d67
AhmedOthman94 <othmanahmed7367@gmail.com>
2023-09-01 07:26:46 -0700

    Whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - that can print program name with a new line
+ * @argc: int
+ * @argv: list
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv[0]);
+	return (0);
+}
